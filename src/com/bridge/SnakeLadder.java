4d192f847c764b59980f1c645f6fcbe5bcd33dad
vamshicherala <vamshikrishna199904@gmail.com>
2021-10-30 00:41:53 +0530

    Added: Snake and ladder game with single player and initial position as 0

---
+++ src/com/bridge/SnakeLadder.java
@@ -0,0 +1,12 @@
+package com.bridge;
+
+import java.util.Scanner;
+
+public class SnakeLadder {
+    public static void main(String[] args) {
+        System.out.println("*****Welcome to Snake and Ladder Stimulator*****");
+        String name = "Vamshi";
+        int playerPosition = 0;
+        System.out.println("Welcome "+name+" Lets Start the Game and your initial position is "+playerPosition);
+    }
+}
