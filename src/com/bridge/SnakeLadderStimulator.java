3465a8ba1fddba2f61a76e21095657b9e77b0160
vamshicherala <vamshikrishna199904@gmail.com>
2021-12-01 18:35:40 +0530

    Added:for loop for player to reach 100 Position

--- src/com/bridge/SnakeLadderStimulator.java
+++ src/com/bridge/SnakeLadderStimulator.java
@@ -10,40 +10,42 @@ import java.util.Random;
  */
 public class SnakeLadderStimulator {
     static int PLAYERPOSITION = 0;
+
     public static void main(String[] args) {
         System.out.println("*****Welcome to Snake and Ladder Stimulator*****");
         String name = "Vamshi";
-        System.out.println("Welcome "+name+" Lets Start the Game and your initial position is : "+ PLAYERPOSITION);
+        System.out.println("Welcome " + name + " Lets Start the Game and your initial position is : " + PLAYERPOSITION);
         SnakeLadderStimulator snakeLadderStimulator = new SnakeLadderStimulator();
-        int dies = snakeLadderStimulator.diesRoll();
-        System.out.println( "DiesRoll number: "+ dies);
-        snakeLadderStimulator.positions(PLAYERPOSITION,dies);
-
+        snakeLadderStimulator.positions();
     }
 
-    int diesRoll(){
-        Random rand = new Random();
-        int least = 1;
-        int number = least + rand.nextInt(6);
-        return number;
-    }
-    void positions(int PLAYERPOSITION, int dies){
-        Random rand1 = new Random();
-        int option = rand1.nextInt(2);
-        switch (option){
-            case 1:
-                System.out.println("**Ladder**");
-                PLAYERPOSITION = PLAYERPOSITION + dies;
-                System.out.println(PLAYERPOSITION);
-                break;
-            case 0:
-                System.out.println("**Snake**");
-                PLAYERPOSITION= PLAYERPOSITION - dies;
-                System.out.println(PLAYERPOSITION);
-                break;
-            default:
-                System.out.println("No Play");
-                System.out.println(PLAYERPOSITION);
+    void positions() {
+        int playerPosition = 0;
+        int min =1;
+        for (int i = 0; PLAYERPOSITION < 100; i++) {
+            Random rand = new Random();
+            int diesRoll = min + rand.nextInt(6);
+            System.out.println("Diesroll number: " + diesRoll);
+            Random rand1 = new Random();
+            playerPosition=PLAYERPOSITION;
+            int option = rand1.nextInt(2);
+            switch (option) {
+                case 1:
+                    System.out.println("**Ladder**");
+                    PLAYERPOSITION = PLAYERPOSITION + diesRoll;
+                    System.out.println(PLAYERPOSITION);
+                    break;
+                case 0:
+                    System.out.println("**Snake**");
+                    PLAYERPOSITION = PLAYERPOSITION - diesRoll;
+                    System.out.println(PLAYERPOSITION);
+                    if (PLAYERPOSITION < 0)
+                        PLAYERPOSITION = 0;
+                    break;
+                default:
+                    System.out.println("No Play");
+                    System.out.println(PLAYERPOSITION);
+            }
         }
     }
 }
