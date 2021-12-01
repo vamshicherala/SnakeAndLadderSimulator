b3b0f0de3c4a39f8978919260ef70bbe0e9d9d70
vamshicherala <vamshikrishna199904@gmail.com>
2021-12-01 16:19:51 +0530

    Added:Ladder,Snake,NoPlay options is added

--- src/com/bridge/SnakeLadderStimulator.java
+++ src/com/bridge/SnakeLadderStimulator.java
@@ -9,12 +9,41 @@ import java.util.Random;
  * in class SnakeLadderStimulator we print a welcome message with player position and single player
  */
 public class SnakeLadderStimulator {
+    static int PLAYERPOSITION = 0;
     public static void main(String[] args) {
         System.out.println("*****Welcome to Snake and Ladder Stimulator*****");
         String name = "Vamshi";
-        int playerPosition = 0;
-        System.out.println("Welcome "+name+" Lets Start the Game and your initial position is : "+playerPosition);
-        DiesRoll diesRoll = new DiesRoll();
-        System.out.println(diesRoll.number);
+        System.out.println("Welcome "+name+" Lets Start the Game and your initial position is : "+ PLAYERPOSITION);
+        SnakeLadderStimulator snakeLadderStimulator = new SnakeLadderStimulator();
+        int dies = snakeLadderStimulator.diesRoll();
+        System.out.println( "DiesRoll number: "+ dies);
+        snakeLadderStimulator.positions(PLAYERPOSITION,dies);
+
+    }
+
+    int diesRoll(){
+        Random rand = new Random();
+        int least = 1;
+        int number = least + rand.nextInt(6);
+        return number;
+    }
+    void positions(int PLAYERPOSITION, int dies){
+        Random rand1 = new Random();
+        int option = rand1.nextInt(2);
+        switch (option){
+            case 1:
+                System.out.println("**Ladder**");
+                PLAYERPOSITION = PLAYERPOSITION + dies;
+                System.out.println(PLAYERPOSITION);
+                break;
+            case 0:
+                System.out.println("**Snake**");
+                PLAYERPOSITION= PLAYERPOSITION - dies;
+                System.out.println(PLAYERPOSITION);
+                break;
+            default:
+                System.out.println("No Play");
+                System.out.println(PLAYERPOSITION);
+        }
     }
 }
